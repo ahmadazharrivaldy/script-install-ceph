f27a454d9a773e391f0e948386224976552b2afe
ahmadazharrivaldy <87871737+ahmadazharrivaldy@users.noreply.github.com>
2025-02-14 10:59:57 +0700

    Create add-ceph-osd.sh

---
+++ add-ceph-osd.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# List of nodes to be processed
+NODES=("ceph-osd01" "ceph-osd02")
+
+# Variable for the storage device
+STORAGE_DEVICE="/dev/vdb"
+
+# Loop through each node
+for NODE in "${NODES[@]}"
+do
+    echo "Copying files to $NODE..."
+    # Copy configuration files and keys to the other node
+    scp /etc/ceph/ceph.conf ${NODE}:/etc/ceph/ceph.conf
+    scp /etc/ceph/ceph.client.admin.keyring ${NODE}:/etc/ceph
+    scp /var/lib/ceph/bootstrap-osd/ceph.keyring ${NODE}:/var/lib/ceph/bootstrap-osd
+
+    echo "Configuring $NODE..."
+    # Access the node and run commands
+    ssh -o StrictHostKeyChecking=no $NODE << EOF
+        # Change ownership of files
+        chown ceph. /etc/ceph/ceph.* /var/lib/ceph/bootstrap-osd/*
+
+        # Create label and partition on the storage device
+        parted --script $STORAGE_DEVICE mklabel gpt
+        parted --script $STORAGE_DEVICE mkpart primary 0% 100%
+
+        # Create LVM with ceph-volume
+        ceph-volume lvm create --data ${STORAGE_DEVICE}1
+EOF
+done
+
+echo "Process completed for all nodes!"
